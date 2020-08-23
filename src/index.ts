5823d58d03d26228dbd135a821b60b347d160427
Sajeev Nair <sajeev.nair@beanworks.com>
2020-08-22 22:51:14 -0700

    user registration and login

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 console.log({ __dirname });
 
@@ -18,7 +19,7 @@ const main = async () => {
 
     const appolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
+            resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false,
         }),
         context: () => ({ em: orm.em })
---
+++ src/resolvers/user.ts
@@ -0,0 +1,136 @@
+import { User } from "../entities/User";
+import { MyContext } from "src/types";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import argon2 from "argon2";
+import { UniqueConstraintViolationException } from "@mikro-orm/core/dist/exceptions";
+
+@InputType()
+class UsernamePasswordInput {
+    @Field()
+    username: string;
+    @Field()
+    password: string;
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: String;
+
+    @Field()
+    message: String;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+
+    @Field(() => User, { nullable: true })
+    user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+    @Mutation(() => UserResponse)
+    async register(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        if (options.username.length < 3) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: "username length must be grater than 2"
+
+                    }
+                ]
+            };
+        }
+
+        if (options.password.length < 3) {
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: "password length must be greater than 2"
+
+                    }
+                ]
+            };
+        }
+
+
+
+        const pwd = await argon2.hash(options.password);
+        const user = em.create(User, { username: options.username, password: pwd });
+
+        try {
+            await em.persistAndFlush(user);
+        } catch (error) {
+            console.log({ error });
+
+            if (error instanceof UniqueConstraintViolationException) {
+                return {
+                    errors: [
+                        {
+                            field: 'username',
+                            message: "This username already exists"
+                        }
+                    ]
+                };
+            }
+
+            return {
+                errors: [
+                    {
+                        field: 'unknown',
+                        message: error.message
+                    }
+                ]
+            };
+        }
+
+        return {
+            user
+        };
+    };
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username });
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: "That username does not exist"
+                    }
+                ]
+            };
+        }
+        const valid = await argon2.verify(user.password, options.password);
+
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: "incorrect password"
+                    }
+                ]
+            };
+        }
+
+        return {
+            user
+        };
+
+    };
+}
+
+
