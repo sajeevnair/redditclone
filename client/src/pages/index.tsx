e9afde3d06ee619d45fd33c4e3d00bffba91a420
Sajeev Nair <sajeev.nair174@gmail.com>
2020-09-25 22:27:26 -0700

    created posts mutation

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { withUrqlClient } from 'next-urql'
 import NavBar from "../components/NavBar"
+import { usePostsQuery } from '../generated/graphql'
 import { createUrqlClient } from '../utils/createUrqlClient'
 
 
-const Index = () =>
-    <>
+const Index = () => {
+    const [{ data }] = usePostsQuery()
+    const queries = data ? data.posts.map(p => <div>{p.title}</div>) : <div>Loading...</div>
+    return (<>
+        {queries}
         <NavBar />
-    </>
+    </>)
+}
 
 export default withUrqlClient(createUrqlClient)(Index)
